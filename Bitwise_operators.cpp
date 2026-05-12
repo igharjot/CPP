abde9032df69a9f22549824f005495590d0d882a
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-12 23:20:06 +0530

    updated bitwise_operators.cpp

--- Bitwise_operators.cpp
+++ Bitwise_operators.cpp
@@ -1,21 +1,38 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    // Fibonacci Series
+    // Bitwise Operators
+    int a=10, b=8;
+    cout << "a&b = " << (a&b) << endl;
+    cout << "~a = " << (~a) << endl;
+    cout << "~b = " << (~b) << endl;
+    cout << "a|b = " << (a|b) << endl;
+    cout << "a^b = " << (a^b) << endl;
+    cout << endl;
 
-    int n,nxt,prev,i,x=0,y=1;
+    // Left & Right Shift Operators
+    cout << "17 >> 1 = " << (17 >> 1) << endl;
+    cout << "17 >> 2 = " << (17 >> 2) << endl;
+    cout << "19 << 1 = " << (19 << 1) << endl;
+    cout << "21 << 3 = " << (21 << 3) << endl;
+    cout << endl;
+
+
+    // Fibonacci Series
+    int n, nxt, prev, i, x = 0, y = 1;
     cout << "enter the number of digits to print fibonacci series : ";
     cin >> n;
 
-    cout << x << endl << y << endl;
+    cout << x << "  " << y << "  ";
     nxt = x + y;
-    for(i = 0 ; i<(n-2) ; i++)
-    {
-        cout << nxt << endl;
+    for (i = 0; i < (n - 2); i++){
+        cout << nxt << "  ";
         prev = nxt;
         nxt = nxt + y;
         y = prev;
     }
+    cout << endl;
     return 0;
 }
