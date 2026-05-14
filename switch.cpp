d590c87ea12c0486589a3edb00773cff0383a390
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-15 00:03:19 +0530

    added switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
-    int 
+    int num = 2;
+    cout << endl;
+    switch (num)
+    {
+    case 1:
+        cout << "First" << endl;
+        break;
+    
+    case 2:
+        cout << "Second" << endl;
+        break;    
+
+    default:
+        cout << "It is defalut case." << endl;
+        break;
+    }
+
     return 0;
 }
