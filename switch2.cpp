bd168341882a198b23ec0c105aa840735f64b2f3
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-17 00:17:25 +0530

    updated switch2.cpp

--- switch2.cpp
+++ switch2.cpp
@@ -1,27 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     // Switch Program to Calculate Minimum Number of Notes to form a Given Amount
 
-    int amount;
-    cout << endl << "Available notes are : $100, $50, $20 and $1" << endl;
+    int amount, hunNotes = 0, fifNotes = 0, tweNotes = 0, oneNotes = 0;
+    cout << endl
+         << "Available notes are : $100, $50, $20 and $1" << endl;
     cout << "Enter the amount : ";
     cin >> amount;
+    int org = amount;
+    int ch = 1;
 
-    switch (amount%100)
+    switch (ch)
     {
-    case 0:
-        
-        break;
     case 1:
-        
-        break;
+        hunNotes = amount / 100;
+        amount %= 100;
+    case 2:
+        fifNotes = amount / 50;
+        amount %= 50;
+    case 3:
+        tweNotes = amount / 20;
+        amount %= 20;
+    case 4:
+        oneNotes = amount / 1;
+        amount %= 1;
 
     default:
-        
+        cout << "Enter valid Amount." << endl;
         break;
     }
+    
+    cout << "Notes Count for the Amount : " << org << endl
+         << endl;
+    cout << "$100 notes = " << hunNotes << endl;
+    cout << "$50 notes = " << fifNotes << endl;
+    cout << "$20 notes = " << tweNotes << endl;
+    cout << "$1 notes = " << oneNotes << endl;
     cout << endl;
 
     return 0;
