9e013b17d8f95b9bd8d27380dd3c0fe1d98c1b8d
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-16 19:25:36 +0530

    added switch2.cpp

--- switch.cpp
+++ switch.cpp
@@ -1,24 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int num = 3;
-    cout << endl;
-    switch (num)
-    {
-    case 1:
-        cout << "First" << endl;
-        break;
+int main()
+{
+    // int num = 3;
+    // cout << endl;
+    // switch (num)
+    // {
+    // case 1:
+    //     cout << "First" << endl;
+    //     break;
+    // case 2:
+    //     cout << "Second" << endl;
+    //     break;
+    // default:
+    //     cout << "Defalut case." << endl;
+    //     break;
+    // }
+    // cout << endl;
+
     
-    case 2:
-        cout << "Second" << endl;
-        break;    
-
-    default:
-        cout << "Defalut case." << endl;
-        break;
-    }
-
+    // Simple Calculator Using Switch
     cout << endl;
+    int x, y;
+    cout << "Enter the value of x : ";
+    cin >> x;
+    cout << endl
+         << "Enter the value of y : ";
+    cin >> y;
+    int ch;
+    cout << endl
+         << "Operations:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Modulas\n";
+    cout << endl
+         << "Enter your choice : ";
+    cin >> ch;
+
+    switch (ch){
+        case 1:
+            cout << x << " + " << y << " = " << (x + y) << endl;
+            break;
+        case 2:
+            cout << x << " - " << y << " = " << (x - y) << endl;
+            break;
+        case 3:
+            cout << x << " * " << y << " = " << (x * y) << endl;
+            break;
+        case 4:
+            if (y == 0){
+                cout << "Division by Zero is not defined.";
+                break;
+            }
+            cout << x << " / " << y << " = " << (x / y) << endl;
+            break;
+        case 5:
+            cout << x << " % " << y << " = " << (x % y) << endl;
+            break;
+        default:
+            cout << "Invalid choice !!!" << endl;
+            break;
+    }
+    cout << endl;
+
     return 0;
 }
