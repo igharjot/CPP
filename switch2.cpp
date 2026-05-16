0a2b7c74593ec53fe3d8270a26f681c4123ca665
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-16 23:30:02 +0530

    updated switch.cpp

---
+++ switch2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Switch Program to Calculate Minimum Number of Notes to form a Given Amount
+
+    int amount;
+    cout << endl << "Available notes are : $100, $50, $20 and $1" << endl;
+    cout << "Enter the amount : ";
+    cin >> amount;
+
+    switch (amount%100)
+    {
+    case 0:
+        
+        break;
+    case 1:
+        
+        break;
+
+    default:
+        
+        break;
+    }
+    cout << endl;
+
+    return 0;
+}
