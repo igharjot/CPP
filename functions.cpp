1e303404dda8eb46c5c87dd0a857683347303ee3
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-23 23:58:08 +0530

    modified functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main(){
     int a,b;
+    cout << "Enter the values of a and b respectively :" <<endl;
     cin >> a >> b;
 
-    cout << pow(a,b) << endl;
+    cout << "b raised to the power a = " << pow(a,b) << endl;
     
     return 0;
 }
