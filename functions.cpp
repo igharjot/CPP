38fcaa254919878c9c5baedb74d91d2be79c72ca
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-24 23:28:26 +0530

    modified functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,12 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int power(int a, int b){
+    int ans = 1;
+
+    for(int i=1 ; i<=b ; i++){
+        ans = ans*a;
+    }
+    
+    return ans;
+}
+
 int main(){
     int a,b;
     cout << "Enter the values of a and b respectively :" <<endl;
     cin >> a >> b;
 
-    cout << "b raised to the power a = " << pow(a,b) << endl;
+    cout << "b raised to the power a = " << power(a,b) << endl;
     
     return 0;
 }
