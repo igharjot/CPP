3e4fb65c1d4050384a5d69f741811ae69e77bf9e
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-25 02:21:54 +0530

    modified functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -18,6 +18,22 @@ bool isEven(int num){
     return 0;
 }
 
+int factorial(int n){
+    int fact = 1;
+    for(int i=1; i<=n; i++){
+        fact = fact*i;
+    }
+    return fact;
+}
+
+int nCr(int n,  int r){
+    int num = factorial(n);
+    int denom = factorial(r) * factorial(n-r);
+    int answer = num/denom;
+
+    return answer;
+}
+
 int main(){
     int a,b;
     cout << "Enter the values of a and b respectively :" <<endl;
@@ -30,14 +46,24 @@ int main(){
     cout << "Enter a number to  check if tis even or odd : ";
     cin >> num; 
 
-    isEven(num);
+    bool result = isEven(num);
 
-    if(isEven){
+    if(result){
         cout << "The entered number if even." << endl ; 
     }
     else{
         cout << "The entered number if odd." << endl;
     }
-    
+
+    cout << endl << "======= Calculating nCr =======" << endl;
+    cout << "Formula : nCr = n! / r! * (n-r)!" << endl << endl;
+
+    int n, r;
+    cout << "Enter the value of n :" ;
+    cin >> n;
+    cout << "Enter the value of r :";
+    cin >> r;
+    cout << n << "C" << r << " = " << nCr(n,r);
+
     return 0;
 }
