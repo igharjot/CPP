56f6e4f84128a2e063b6ea5069b02422e648c442
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-25 17:36:43 +0530

    completed functions2.cpp

--- functions2.cpp
+++ functions2.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 void ap(){
-    cout << "===============================================================================\n" << endl;
-    cout << "    Program - 1 : Arithematic Progression (Formula for nth Term: (3*n + 7))    \n" << endl;
+    cout << "\n===============================================================================" << endl;
+    cout << "          Program - 1 : Arithematic Progression (Formula : (3*n + 7))          " << endl;
     cout << "===============================================================================" << endl;
     int n, result;
     cout << "Enter the value of n : ";
@@ -12,40 +12,64 @@ void ap(){
     cout << "The nth term of the given AP is : " << result << endl << endl;
 }
 
-// total no of set bits in a and b (calculatenumber of 1's in its binary forms)
 void calcSetBits(){
-    cout << "===============================================================================\n" << endl;
-    cout << "           Program - 2 : Calculate Total nNo. of Set Bits in a and b           \n" << endl;
+    cout << "\n===============================================================================" << endl;
+    cout << "           Program - 2 : Calculate Total nNo. of Set Bits in a and b           " << endl;
     cout << "===============================================================================" << endl;
     int a, b;
-    cout << "Enter the values of a and b :";
+    cout << "Enter the values of a and b : ";
     cin >> a >> b;
-
+    int countA = 0, countB = 0;
+    while(a > 0){
+        if(a % 2 == 1){
+            countA++;
+        }
+        a /= 2;
+    }
+    while (b > 0){
+        if (b % 2 == 1){
+            countB++;
+        }
+        b /= 2;
+    }
+    int count = countA + countB;
+    cout << "Total Number of Set Bits in Both a and b are : " << count << endl;
     cout << endl;
 }
 
 void fibonacci(){
-    cout << "===============================================================================\n" << endl;
-    cout << "               Program - 3 : Fibonacci Series (Find the nth term)              \n" << endl;
+    cout << "\n===============================================================================" << endl;
+    cout << "               Program - 3 : Fibonacci Series (Find the nth term)              " << endl;
     cout << "===============================================================================" << endl;
     int n;
     cout << "Enter the value of n : ";
     cin >> n;
     int prev = 0, curr = 1;
     int next;
-    for(int i = curr; i<n ; i++){
-        next = curr + prev;
-        
+    if(n == 0){
+        cout << "Invalid value !! (Enter n > 0)" << endl;
     }
-    cout << "The nth term of the Fibonacci Series is : " << curr << endl;
+    else if(n == 1){
+        cout << "The 1st term of the Fibonacci Series is : 0" << endl;
+    }
+    else if (n == 2){
+        cout << "The 2nd term of the Fibonacci Series is : 1" << endl;
+    }
+    else{
+        for(int i = prev; i<(n-2) ; i++){
+            next = curr + prev;
+            prev = curr;
+            curr = next;       
+        }
+        cout << "The term " << n << " of the Fibonacci Series is : " << curr << endl;
+    }
     cout << endl;
 }
 
 int main(){
-    
     ap();
-    // calcSetBits();
-    // fibonacci();
-    
+    calcSetBits();
+    fibonacci();
+
     return 0;
 }
