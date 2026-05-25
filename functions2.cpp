a27e741517b0e2fd9a91dd35845c286f961b4031
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-25 16:59:51 +0530

    added functions2.cpp

--- functions2.cpp
+++ functions2.cpp
@@ -1,31 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+void ap(){
+    cout << "===============================================================================\n" << endl;
+    cout << "    Program - 1 : Arithematic Progression (Formula for nth Term: (3*n + 7))    \n" << endl;
     cout << "===============================================================================" << endl;
-    cout << "    Program - 1 : Arithematic Progression (Formula for nth Term: (3*n + 7))    \n"<<endl;
-    cout << "===============================================================================" << endl;
-
-
-
-
-
-    cout << "===============================================================================" << endl;
-    cout << "           Program - 2 : Calculate Total nNo. of Set Bits in a and b           \n"
-         << endl;
-    cout << "===============================================================================" << endl;
-
-
-
-
-    cout << "===============================================================================" << endl;
-    cout << "               Program - 3 : Fibonacci Series (Find the nth term)              \n"
-         << endl;
-    cout << "===============================================================================" << endl;
-    return 0;
+    int n, result;
+    cout << "Enter the value of n : ";
+    cin >> n;
+    result = 3*n + 7;
+    cout << "The nth term of the given AP is : " << result << endl << endl;
 }
 
-
-// ap = 3*n + 7
 // total no of set bits in a and b (calculatenumber of 1's in its binary forms)
-// fibonacci series(print nth term)
+void calcSetBits(){
+    cout << "===============================================================================\n" << endl;
+    cout << "           Program - 2 : Calculate Total nNo. of Set Bits in a and b           \n" << endl;
+    cout << "===============================================================================" << endl;
+    int a, b;
+    cout << "Enter the values of a and b :";
+    cin >> a >> b;
+
+    cout << endl;
+}
+
+void fibonacci(){
+    cout << "===============================================================================\n" << endl;
+    cout << "               Program - 3 : Fibonacci Series (Find the nth term)              \n" << endl;
+    cout << "===============================================================================" << endl;
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+    int prev = 0, curr = 1;
+    int next;
+    for(int i = curr; i<n ; i++){
+        next = curr + prev;
+        
+    }
+    cout << "The nth term of the Fibonacci Series is : " << curr << endl;
+    cout << endl;
+}
+
+int main(){
+    
+    ap();
+    // calcSetBits();
+    // fibonacci();
+    
+    return 0;
+}
