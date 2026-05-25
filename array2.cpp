6017aa842ae874d5ae276bc1a19b57a76232713d
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-26 01:46:24 +0530

    created array2.cpp

---
+++ array2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArr(int arr[], int size){
+    for (int i = 0; i < size; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+
+
+    cout << endl;
+    return 0;
+}
