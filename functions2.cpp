36e6df465bc8afc0fab24e46fb15e8cb691f6b36
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-25 13:59:45 +0530

    modified functions2.cpp

---
+++ functions2.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "===============================================================================" << endl;
+    cout << "    Program - 1 : Arithematic Progression (Formula for nth Term: (3*n + 7))    \n"<<endl;
+    cout << "===============================================================================" << endl;
+
+
+
+
+
+    cout << "===============================================================================" << endl;
+    cout << "           Program - 2 : Calculate Total nNo. of Set Bits in a and b           \n"
+         << endl;
+    cout << "===============================================================================" << endl;
+
+
+
+
+    cout << "===============================================================================" << endl;
+    cout << "               Program - 3 : Fibonacci Series (Find the nth term)              \n"
+         << endl;
+    cout << "===============================================================================" << endl;
+    return 0;
+}
+
+
+// ap = 3*n + 7
+// total no of set bits in a and b (calculatenumber of 1's in its binary forms)
+// fibonacci series(print nth term)
