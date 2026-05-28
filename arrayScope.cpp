13127bc9a72402114bb0254c2cdcb4987a68d661
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-28 21:52:32 +0530

    updated arrayScope.cpp

--- arrayScope.cpp
+++ arrayScope.cpp
@@ -2,23 +2,33 @@
 using namespace std;
 
 void update(int arr[], int n){
-    cout << "Inside the Function" << endl;
+    cout << "\n===== INSIDE UPDATE FUNCTION =====" << endl << endl;
+    cout << "Updating the first element of the array...." << endl;
+    arr[0] = 99;
+    cout << "Updated Array : ";
     for (int i = 0; i < n ; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
-    cout << "Going Back to the Main Function" << endl;
+    cout << "\n=== GOING BACK TO MAIN FUNCTION ===\n" << endl;
 }
 
 int main(){
     int arr[3] = {10,20,30};
     int size = sizeof(arr)/sizeof(int);
+    cout << endl <<"Original Array : ";
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     update(arr, size);
 
+    cout << "Re-printing Array in main() : ";
     for(int i=0; i<size; i++){
         cout << arr[i] << " " ;
     }
 
-    cout << endl;
+    cout << endl << endl;
     return 0;
 }
