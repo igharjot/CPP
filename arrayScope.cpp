86453c35e0b9613b9afd08f90ece40f1609e57d0
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-28 19:28:36 +0530

    updated arrayScope.cpp

--- arrayScope.cpp
+++ arrayScope.cpp
@@ -1,8 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void update(int arr[], int n){
+    cout << "Inside the Function" << endl;
+    for (int i = 0; i < n ; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "Going Back to the Main Function" << endl;
+}
+
 int main(){
-    
+    int arr[3] = {10,20,30};
+    int size = sizeof(arr)/sizeof(int);
+    update(arr, size);
+
+    for(int i=0; i<size; i++){
+        cout << arr[i] << " " ;
+    }
 
     cout << endl;
     return 0;
