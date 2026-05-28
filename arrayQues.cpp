7619463d67ac4ef0ce555a4f8abb265265154aef
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-28 22:43:37 +0530

    added linear search code in  arrayQues.cpp

--- arrayQues.cpp
+++ arrayQues.cpp
@@ -9,12 +9,35 @@ void printArr(int arr[], int size){
     cout << endl;
 }
 
-int arraySum(int arr[], int size){
+void arraySum(int arr[], int size){
+    cout << "\n======= Sum of Array Elements =======\n" << endl;
     int sum = 0;
     for(int i=0; i<size; i++){
         sum += arr[i];
     }
-    return sum;
+    cout << "Sum of the elements of the array is : " << sum << endl;
+}
+
+void linearSearch(int arr[], int size){
+    cout << "\n=========== Linear Search ==========\n" << endl;
+    int n;
+    int idx = -1;
+    cout << "Enter the element to search in the array : ";
+    cin >> n;
+    cout << "Searching " << n << " in the array...\n" << endl;
+    for(int i=0; i<size; i++) {
+        if(arr[i] == n){
+            idx = i;
+            break;
+        } else{
+            continue;
+        }
+    }
+    if(idx == -1){
+        cout << "Element not found.\n" << endl;
+    } else{
+        cout << "Element found in the array at index : " << idx << endl;
+    }
 }
 
 int main(){
@@ -26,9 +49,10 @@ int main(){
     for(int i=0; i<size; i++){
         cin >> arr[i];
     }
-    cout << endl;
     printArr(arr, size);
-    cout << "Sum of the elements of the array is : " << arraySum(arr, size) << endl;
+    arraySum(arr, size);
+    linearSearch(arr, size);
+    
     cout << endl;
     return 0;
 }
