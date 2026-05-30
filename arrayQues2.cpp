2cb4990342feb43672af1ba77b396b5f670f0d8f
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-30 16:30:12 +0530

    created arrayQues2.cpp

---
+++ arrayQues2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArr(int arr[], int size){
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void swapAdjacent(int arr[], int size){
+    for(int i=0; i<size; i+=2){
+        if((i+1) < size){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    printArr(arr, size);
+}
+
+int main() {
+    int arr[50];
+    int size;
+    cout << "\nEnter the size of the array : ";
+    cin >> size;
+    cout << "Enter the elements into the array : ";
+    for (int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+    cout << "Printing the Array : ";
+    printArr(arr, size);
+    swapAdjacent(arr, size);
+    cout << endl;
+    return 0;
+}
