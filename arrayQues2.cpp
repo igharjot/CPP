330752c0e32c541906afcae1e56a7dbc5dddc558
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-05-30 16:32:56 +0530

    updated arrayQues2.cpp

--- arrayQues2.cpp
+++ arrayQues2.cpp
@@ -9,6 +9,7 @@ void printArr(int arr[], int size){
 }
 
 void swapAdjacent(int arr[], int size){
+    cout << "\n======= Swapping Adjacent Elements of the Array =======\n" << endl;
     for(int i=0; i<size; i+=2){
         if((i+1) < size){
             swap(arr[i],arr[i+1]);
