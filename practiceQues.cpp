814fbb7413fb1ce2af8b69998d8c214a216b85fc
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-08-09 23:52:32 +0530

    minor changes

--- practiceQues.cpp
+++ practiceQues.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void countWords(vector<string> words){
-    
-}
+// void countWords(vector<string> words){
+//     for(auto st = words.begin(); st <= words.end() ; st++){
+//         cout << st << endl;
+//     }
+// }
 
 int main() {
     string line;
     cout << "Enter a string : ";
     cin >> line;
-    cout << endl;
     vector<string> words;
     string word = "";
 
     for(char ch : line){
+        cout << ch << ", ";
         if(ch == ' '){
             cout << "The word is : " << word << endl;
             words.push_back(word);
@@ -23,8 +25,10 @@ int main() {
         else{
             word += ch;
         }
+        cout << word << endl;
     }
-    countWords(words);
+
+    // countWords(words);
 
     cout << endl;
     return 0;
