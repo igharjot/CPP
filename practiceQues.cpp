2e91182f79a3deb45808ae82a2aab010b892700b
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-08-20 20:39:12 +0530

    euclidean algorithm to find gcd/hcf

--- practiceQues.cpp
+++ practiceQues.cpp
@@ -8,34 +8,58 @@ using namespace std;
 // }
 
 void findGCD(int x, int y){
-    cout << x << " , " << y << endl;
+    while (x > 0 && y > 0){
+        cout << "x = " << x << " and y = " << y << endl;  
+        if (x > y){
+            x = x % y;
+        } else if (y > x){
+            y = y % x;
+        } else{
+            cout << "\n Ans: The GCD or HCF of " << x << " & " << y << " is: " << x << endl;
+            return;
+        }
+    }
+    if(x == 0){
+        cout << "\n Ans: The GCD or HCF of " << x << " & " << y << " is: " << y << endl;
+    } else if(y == 0){
+        cout << "\n Ans: The GCD or HCF of " << x << " & " << y << " is: " << x << endl;
+    }
 }
 
-int main() {
+int main()
+{
     string line;
     cout << "Enter a string : ";
     cin >> line;
     vector<string> words;
     string word = "";
 
-    for(char ch : line){
+    for (char ch : line)
+    {
         tolower(ch);
         cout << ch << ", ";
-        if(ch == ' '){
+        if (ch == ' ')
+        {
             cout << "The word is : " << word << endl;
             words.push_back(word);
             word = "";
             continue;
         }
-        else{
+        else
+        {
             word += ch;
         }
         cout << word << endl;
     }
 
-    // countWords(words); 
+    // countWords(words);
 
-    findGCD(num1, num2);
+    int n1, n2;
+    cout << "\nFinding GCD/HCF between 2 numbers.\n";
+    cout << "Enter 2 numbers: ";
+    cin >> n1 >> n2;
+
+    findGCD(n1, n2);
 
     cout << endl;
     return 0;
