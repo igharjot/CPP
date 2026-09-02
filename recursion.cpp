774c549cc58a0ffd8f5b020a5f6414fb74a0c2c8
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-09-02 22:50:22 +0530

    added recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print(int count, int n){
+    if(count == n+1) return;
+    cout << count << "  ";
+    count++;
+    print(count, n);
+}
+
 int main() {
-    
+    int n ;
+    int count = 0;
+    cout << "Enter a number to count upto using Recursion: ";
+    cin >> n;
+    print(count, n);
     cout << endl;
     return 0;
 }
