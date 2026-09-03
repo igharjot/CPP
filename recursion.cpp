c716e5280473b7cc576b10eb7c7a34ccfcc9fb5b
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-09-03 11:52:18 +0530

    uppdated recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,19 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int count, int n){
-    if(count == n+1) return;
+void OneToN(int count, int n){
+    if(count > n) return;
     cout << count << "  ";
-    count++;
-    print(count, n);
+    OneToN(count+1, n);
+}
+
+void NToOne(int n, int i){
+    if(n < i) return;
+    cout << n << "  ";
+    NToOne(n-1,i);
 }
 
 int main() {
     int n ;
-    int count = 0;
     cout << "Enter a number to count upto using Recursion: ";
     cin >> n;
-    print(count, n);
+    OneToN(1, n);
+    cout << endl;
+    NToOne(n, 1);
     cout << endl;
     return 0;
 }
