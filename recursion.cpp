1599bb82cece6f1bd72cd7b5a968fce0bf46b216
Harjot Singh <145702629+igharjot@users.noreply.github.com>
2026-09-04 20:22:37 +0530

    updated recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -13,13 +13,44 @@ void NToOne(int n, int i){
     NToOne(n-1,i);
 }
 
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " , ";
+    }
+    cout << endl;
+}
+
+void reverseArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
+}
+
 int main() {
     int n ;
     cout << "Enter a number to count upto using Recursion: ";
     cin >> n;
+
     OneToN(1, n);
     cout << endl;
+    
     NToOne(n, 1);
     cout << endl;
+    
+    int size;
+    cout << "Enter size of the array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter element into tnnhe array: ";
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+    cout << "Your array is : ";
+    printArray(arr, size);
+    
+    reverseArray(arr, size);
+    
+    
     return 0;
 }
